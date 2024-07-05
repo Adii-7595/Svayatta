6701b52470bee79d760840dbb986056e69e4a0b4
Aditya Gupta <rajaditya2399@gmail.com>
2024-07-05 13:03:46 +0530

    some css

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
       <div className="sideBar">
         <div className="upperside">
           <div className="upperSideTop">
-            <img src={gptLogo} alt="Logo" className='logo' />
+            <img src={gptLogo} alt="Logo" classNazme='logo' />
             <span className='brand'>Svayatta GPT</span>
           </div>
           <button className='midBtn'>
